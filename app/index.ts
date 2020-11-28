f1b16bc739c3772f75cc92ae6c885c023e0e22d4
Ivan Grishin <r@exah.me>
2020-11-28 13:05:11 +0000

    ✨ Add CORS

--- app/index.ts
+++ app/index.ts
@@ -1,9 +1,11 @@
 import Koa from 'koa'
+import cors from '@koa/cors'
 import { handleError } from './middleware/handle-error'
 import { router } from './router'
 
 export const app = new Koa()
 
+app.use(cors())
 app.use(handleError)
 app.use(router.routes())
 app.use(router.allowedMethods())
