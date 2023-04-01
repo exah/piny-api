6c99344994ea6c0f27558fe4d123ec5a4340ebbf
John Grishin <r@exah.me>
2023-04-01 18:46:18 +0100

    ⬆️ Update all deps

--- app/middleware/handle-error.ts
+++ app/middleware/handle-error.ts
@@ -1,17 +1,20 @@
-import { v4 as UUID } from 'uuid'
 import { Context, Next } from 'koa'
+import crypto from 'crypto'
 import { ResponseError } from '../utils/errors'
 
 export async function handleError(context: Context, next: Next) {
   try {
     await next()
   } catch (error) {
-    const id = UUID()
-    console.error(Object.assign(error, { id }))
+    const id = crypto.randomUUID()
 
-    if (error instanceof ResponseError) {
-      context.status = error.status
-      context.body = { id, message: error.description || error.message }
+    if (error instanceof Error) {
+      console.error(Object.assign(error, { id }))
+
+      if (error instanceof ResponseError) {
+        context.status = error.status
+        context.body = { id, message: error.description || error.message }
+      }
     } else {
       context.status = 500
       context.body = { id, message: '😭 Something went wrong' }
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,5 +1,7 @@
 import { RouterContext as BaseRouterContext } from '@koa/router'
 
-export interface RouterContext<P = any, S = {}> extends BaseRouterContext<S> {
-  params: P
+export interface RouterContext<Params = unknown, State = unknown>
+  extends Omit<BaseRouterContext, 'params' | 'state'> {
+  params: Params
+  state: State
 }
