c9138ba45ca5e355053523b7e6cc414702166614
John Grishin <r@exah.me>
2023-04-01 23:48:01 +0100

    🚚 Move app initialisation to server.ts

--- app/index.ts
+++
@@ -1,9 +0,0 @@
-import Koa from 'koa'
-import { handleError } from './middleware/handle-error'
-import { router } from './router'
-
-export const app = new Koa()
-
-app.use(handleError)
-app.use(router.routes())
-app.use(router.allowedMethods())
--- server.ts
+++ server.ts
@@ -1,8 +1,15 @@
-import { app } from './app'
+import Koa from 'koa'
+import { handleError } from './app/middleware/handle-error'
+import { router } from './app/router'
 import './app/data-source'
 
 const hostname = process.env.HOST || '0.0.0.0'
 const port = Number(process.env.PORT) || 3000
+const app = new Koa()
+
+app.use(handleError)
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.listen({ hostname, port })
 
