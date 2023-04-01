b3c8a9ec63c887acb7b6f0b3a560b77b7eee14c6
John Grishin <r@exah.me>
2023-04-01 23:57:21 +0100

    🧹 Remove unused options from `DataSource`

--- app/data-source.ts
+++ app/data-source.ts
@@ -4,8 +4,6 @@ export const dataSource = new DataSource({
   type: 'sqlite',
   database: 'db/piny.db',
   entities: ['app/entities/**/*'],
-  migrations: ['app/migrations/**/*'],
-  subscribers: ['app/subscribers/**/*'],
 })
 
 await dataSource.initialize()
