059d871cc11c614d337677d435af052fa2844108
John Grishin <r@exah.me>
2023-04-01 23:22:54 +0100

    ➖ Remove cors

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,9 @@
 import Koa from 'koa'
-import cors from '@koa/cors'
 import { handleError } from './middleware/handle-error'
 import { router } from './router'
 
 export const app = new Koa()
 
-app.use(cors())
 app.use(handleError)
 app.use(router.routes())
 app.use(router.allowedMethods())
