7e3f9899f6182dd75e979d40831a14f00f25ad16
John Grishin <r@exah.me>
2025-11-26 00:34:28 +0000

    🔧 Log errors classic way

--- packages/api/app.ts
+++ packages/api/app.ts
@@ -1,22 +1,10 @@
 import Koa from 'koa'
-import logger from 'koa-pino-logger'
-import pretty from 'pino-pretty'
 import { catchErrors } from '@piny/status/middlewares'
 import { routes } from './routes'
 import { getRouterContext } from './middlewares'
 import type { RouterSessionState, RouterContext } from './types/router'
 
 export const app = new Koa<RouterSessionState, RouterContext<unknown>>()
-
-app.use(
-  logger(
-    pretty({
-      colorize: true,
-      hideObject: true,
-      minimumLevel: 'error',
-    })
-  )
-)
 
 app.use(getRouterContext)
 app.use(catchErrors)
