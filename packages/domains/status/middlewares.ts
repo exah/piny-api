3881cc9cf9cb620b3ce16637973b54c98454daf1
John Grishin <r@exah.me>
2025-11-26 14:30:53 +0000

    🔧 Increase log depth

--- packages/domains/status/middlewares.ts
+++ packages/domains/status/middlewares.ts
@@ -34,8 +34,6 @@ export async function catchErrors(
     const id = await createErrorId()
     const responseError = getResponseError(error)
 
-    console.error(responseError)
-
     context.response.status = responseError.status
     context.response.body = {
       id,
--- packages/tools/logger.ts
+++ packages/tools/logger.ts
@@ -1,4 +1,4 @@
 import { pino } from 'pino'
 import pretty from 'pino-pretty'
 
-export const logger = pino(pretty())
+export const logger = pino({ depthLimit: 10 }, pretty())
