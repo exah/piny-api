74a4820b707c03373f1c39d4e16a927835eb79f6
John Grishin <r@exah.me>
2025-11-26 14:59:38 +0000

    ♻️ Improve valibot issue message report

--- packages/domains/status/middlewares.ts
+++ packages/domains/status/middlewares.ts
@@ -1,20 +1,20 @@
 import { Next } from 'koa'
-import { isValiError } from 'valibot'
+import * as v from 'valibot'
 import { RouterContext } from '@piny/api/types/router'
 import { BadRequest, SomethingWentWrong } from './errors'
 import { createErrorId, isResponseError } from './utils'
 import type { ErrorResponse } from './types'
 
+function getReadableIssueMessage(issue: v.BaseIssue<unknown>) {
+  return issue.path
+    ? `'${issue.path.map((path) => path.key).join(',')}:' ${issue.message}`
+    : issue.message
+}
+
 function getResponseError(error: unknown) {
-  if (isValiError(error)) {
+  if (v.isValiError(error)) {
     return new BadRequest(
-      `🤦‍♂️ Bad request: ${error.issues
-        .map((issue) =>
-          issue.path
-            ? `${issue.path.join(',')} ${issue.message}`
-            : issue.message
-        )
-        .join(', ')}`,
+      `🤦‍♂️ Bad request: ${error.issues.map(getReadableIssueMessage).join(', ')}`,
       { cause: error, meta: error.issues }
     )
   } else if (isResponseError(error)) {
