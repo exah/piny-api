8c8af040e2df7cb92122453d1e21484dea1339b6
John Grishin <r@exah.me>
2025-12-06 12:41:13 +0000

    🎨 Prettify log output

--- packages/api/server.ts
+++ packages/api/server.ts
@@ -1,11 +1,13 @@
 import '@piny/db/init'
+import dedent from 'dedent'
+import { logger } from '@piny/tools/logger'
 import { getServerHostPort, getServerURL } from './config'
 import { app } from './app'
 
 const { host, port } = getServerHostPort()
 export const server = app.listen({ host, port })
 
-console.log(`
+logger.info(dedent`
   🌲 Welcome to Piny
   Server is open at ${getServerURL({ host, port })}
 `)
--- packages/tools/logger.ts
+++ packages/tools/logger.ts
@@ -1,4 +1,7 @@
 import { pino } from 'pino'
 import pretty from 'pino-pretty'
 
-export const logger = pino({ depthLimit: 10 }, pretty())
+export const logger = pino(
+  { depthLimit: 10 },
+  pretty({ ignore: 'pid,hostname' })
+)
